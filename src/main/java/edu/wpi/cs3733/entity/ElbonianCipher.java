e3fdf9e1d915931a80092b01480cd00d67b5bca5
karlehlers <karl.ehlers@gmail.com>
2020-02-22 19:13:49 -0500

    Created elbonian cipher, known "S" problem

--- src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
+++ src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
@@ -4,17 +4,42 @@ package edu.wpi.cs3733.entity;
  * Elbonian cipher; you must implement the cipher and the observer pattern
  */
 public class ElbonianCipher implements Observer {
+	char[] alphabet ={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	private String text;
 
 	public void setText(String text){
-
+		this.text = text;
 	}
 
 	public String getText(){
-		return null;
+		return text;
 	}
 
 	@Override
 	public void notify(Object object){
+		setText(encode((String) object));
+	}
 
+	public String encode(String toEncode){
+		String toReturn = "";
+		for(int i=0; i < toEncode.length(); i++){
+			for(int q=0; q < alphabet.length; q++){
+				boolean foundMatch = false;
+				if((toEncode.toUpperCase().charAt(i)) == alphabet[q]){
+					toReturn += String.format("%02d", (q+1));
+					foundMatch = true;
+				}
+				else if((toEncode.charAt(i) == (' '))){
+					toReturn += "S";
+					foundMatch = true;
+				}
+				else if(foundMatch = false){
+					System.out.println("Problem");
+				}
+			}
+
+		}
+		return toReturn;
 	}
 }
